73dbffd9f19f8d3957da72a6887e0c5e353a01aa
Sebastian Jeong <sebastianrcnt@gmail.com>
2022-03-22 14:17:16 +0900

    :sparkles: feat: add redirect for logged out users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
     include ApplicationHelper
     def index
+        # 로그인 안 되었을 때에는 시작 스크린으로
+        if not is_logged_in
+            redirect_to start_path
+        end
+
         @user_info = get_user_info
     end
 
