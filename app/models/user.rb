66ab39f54301687339191535b6a87523a455553a
Sebastian Jeong <sebastianrcnt@gmail.com>
2022-03-23 15:32:15 +0900

    :bug: fix Appointment.all to all.select
    - Appointment.all.select { |a| do_something }

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
     def get_participating_appointments
         return(
-            Appointment.all do |appointment|
+            Appointment.all.select do |appointment|
                 appointment.get_participants_set.include? self
             end
         )
